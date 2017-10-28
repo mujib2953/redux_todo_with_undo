674b1229c9696a95de5b33ef13264a09b937d322
Mujib <ansarimujiburrehman@gmail.com>
2017-10-28 14:26:37 +0530

    Added Redux code

--- src/index.js
+++ src/index.js
@@ -2,6 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import reducer from './reducers';
+
 import MainComp from './Components/MainComp/MainComp';
 
-ReactDOM.render(<MainComp />, document.getElementById('root'));
+const store = createStore( reducer );
+
+ReactDOM.render(
+    <Provider store={ store } >
+        <MainComp />
+    </Provider>, 
+    document.getElementById('root')
+);
